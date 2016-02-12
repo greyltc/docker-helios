8b1980329c47751f431c9bb1a9d71edb5ffe8ffe
Grey Christoforo <grey@christoforo.net>
2016-02-12 09:20:05 +0000

    copy reset instead of generating it

--- setup-helios.sh
+++ setup-helios.sh
@@ -7,14 +7,7 @@ cd helios-server
 sed -i 's,kombu==3.0.26,kombu==3.0.30,g' requirements.txt
 pip2 install -r requirements.txt
 
-cat <<EOF > reset.sh
-#!/usr/bin/env bash
-dropdb helios
-createdb helios
-python2 manage.py syncdb
-python2 manage.py migrate
-echo "from helios_auth.models import User; User.objects.create(user_type='google',user_id='grey@christoforo.net', info={'name':'Grey Christoforo'})" | python2 manage.py shell
-EOF
+cp /reset.sh reset.sh
 chmod +x reset.sh
 
 su postgres -c 'pg_ctl -s -D /var/lib/postgres/data start -w -t 120'
